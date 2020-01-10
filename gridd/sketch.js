f6492589926800a89bb63cf6e44d9d014acb5ede
winutta <willinutter@gmail.com>
2020-01-09 19:06:11 -0800

    Update sketch.js

--- gridd/sketch.js
+++ gridd/sketch.js
@@ -1,32 +1,48 @@
 var cols, rows;
-var scl = 4;
-var w = 800;
-var h = 800;
+var scl = 8;
+var w = 2000;
+var h = 1300;
 var heights = [];
 var flyingX = 0;
 var flyingY = 0;
+let shaderBg;
+let theShader;
+function preload() {
+  // load the shader
+  theShader = loadShader("shader.vert", "shader.frag");
+}
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight,WEBGL);
-  
+  noStroke();
+  shaderBg = createGraphics(window.innerWidth, window.innerHeight, WEBGL);
+  rectMode(CENTER);
+  // turn off the cg layers stroke
+  shaderBg.noStroke();
   cols = w/scl;
   rows = h/scl;
   //console.log("Starting");
   for (let i = 0; i< rows;i++){
     heights.push([]);
+    
   }
   
   
 }
 
 function draw() {
+  shaderBg.shader(theShader);
+  
+  theShader.setUniform("u_resolution", [width, height]);
+  shaderBg.rect(0, 0, width, height);
+  
   background(0);
   //stroke(255);
-  noStroke();
-  noFill();
+  // noStroke();
+  // noFill();
   rotateX(PI/3);
   translate(-h/2, -w/2)
-  orbitControl();
+  // orbitControl();
   let horiz = norms(map(mouseX, 0, width,0.1,-0.1),0.1);
   let up = norms(map(mouseY, 0, height,0.1,-0.1),0.1);
   flyingX -= horiz;
@@ -44,21 +60,23 @@ function draw() {
     //console.log(heights[x])
     //ightsx.push(random(-10,10))
     //console.log(heights[x][y]);
-    xoff += scl/300;
+    xoff += scl/200;
    }
-    yoff += scl/300;  
+    yoff += scl/200;  
   }
+  
   for (var y = 0; y< cols-1; y++) {
     //let col = map(y/(cols-1),0,1,0,255);
-    
+    texture(shaderBg);
     beginShape(TRIANGLE_STRIP);
+    
     for (var x = 0; x<rows-1; x++){
-      let hval = map(heights[x][y],-150,150,0,255)
-      fill(0,hval,255-hval);
+      //fill(0,map(heights[x][y],-150,150,0,255),map(heights[x][y],-150,150,0,255));
       //console.log(heights[x][y+1]);
       //console.log(x,y)
-      vertex(x*scl,y*scl-300,heights[x][y]-100);
-      vertex(x*scl,(y+1)*scl-300,heights[x][y+1]-100);
+      
+      vertex(x*scl,y*scl-300,heights[x][y],map(heights[x][y],-150,150,0,1),0);
+      vertex(x*scl,(y+1)*scl-300,heights[x][y+1],map(heights[x][y+1],-150,150,0,1),0);
    }
     endShape();
   }
