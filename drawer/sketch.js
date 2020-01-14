a4d74f7dfe417b0798612418b498d8853b722159
winutta <willinutter@gmail.com>
2020-01-13 17:33:28 -0800

    Update sketch.js

--- drawer/sketch.js
+++ drawer/sketch.js
@@ -2,6 +2,21 @@ let ppmouseX = 0;
 let ppmouseY = 0;
 let pppmouseX = 0;
 let pppmouseY = 0;
+
+var firstMove;
+
+window.addEventListener('touchstart', function (e) {
+    firstMove = true;
+});
+
+window.addEventListener('touchmove', function (e) {
+    if (firstMove) {
+        e.preventDefault();
+
+        firstMove = false;
+    }
+});
+
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
   background(255);
