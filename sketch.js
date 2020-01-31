df7d9e585321354731c731bbd36fa2b52f12a2f9
winutta <willinutter@gmail.com>
2020-01-31 12:35:53 -0800

    Update sketch.js
    add event listening and make smoother squiggles

--- sketch.js
+++ sketch.js
@@ -5,6 +5,21 @@ let pppmouseX = 0;
 let pppmouseY = 0;
 let d0 = 0;
 let d1 = 0;
+
+var firstMove;
+
+window.addEventListener('touchstart', function (e) {
+    firstMove = true;
+});
+
+window.addEventListener('touchmove', function (e) {
+    if (firstMove) {
+        e.preventDefault();
+
+        firstMove = false;
+    }
+},{passive: false});
+
 function setup() {
   //print(window.innerWidth,window.innerHeight);
   canvas = createCanvas(window.innerWidth, window.innerHeight-120);
@@ -24,15 +39,24 @@ function draw() {
   beginShape();
   
   if (mouseIsPressed) {
-    point(mouseX,mouseY);
+//     point(mouseX,mouseY);
+//     vertex(ppmouseX, ppmouseY); // first point
+//     d0 = pow(pow(ppmouseX-pppmouseX,2)+pow(ppmouseY-pppmouseY,2),0.5)/100;
+//     d1 =  pow(pow(pmouseX-mouseX,2)+pow(pmouseY-mouseY,2),0.5)/100;
+//     //bezierVertex((2*ppmouseX-pppmouseX),(2*ppmouseY-pppmouseY), (2*pmouseX - mouseX), (2*pmouseY - mouseY),pmouseX, pmouseY);
+//     bezierVertex((ppmouseX+(ppmouseX-pppmouseX)*1/d0),(ppmouseY+(ppmouseY-pppmouseY)*1/d0), (pmouseX + (pmouseX-mouseX)*1/d1), (pmouseY + (pmouseY-mouseY)*1/d1),pmouseX, pmouseY);
+//   //bezierVertex(25, 125, 75, 140, 120, 120);
+//     endShape();
     vertex(ppmouseX, ppmouseY); // first point
-    d0 = pow(pow(ppmouseX-pppmouseX,2)+pow(ppmouseY-pppmouseY,2),0.5)/100;
+    d0 = pow(pow(pmouseX-ppmouseX,2)+pow(pmouseY-ppmouseY,2),0.5)/100;
+    //d0 = 1.5;
     d1 =  pow(pow(pmouseX-mouseX,2)+pow(pmouseY-mouseY,2),0.5)/100;
+    //d1 = 1.5;
     //bezierVertex((2*ppmouseX-pppmouseX),(2*ppmouseY-pppmouseY), (2*pmouseX - mouseX), (2*pmouseY - mouseY),pmouseX, pmouseY);
-    bezierVertex((ppmouseX+(ppmouseX-pppmouseX)*1/d0),(ppmouseY+(ppmouseY-pppmouseY)*1/d0), (pmouseX + (pmouseX-mouseX)*1/d1), (pmouseY + (pmouseY-mouseY)*1/d1),pmouseX, pmouseY);
+    bezierVertex((pmouseX+(pmouseX-ppmouseX)*1/d0),(pmouseY+(pmouseY-ppmouseY)*1/d0), (pmouseX + (pmouseX-mouseX)*1/d1), (pmouseY + (pmouseY-mouseY)*1/d1),pmouseX, pmouseY);
+    //bezierVertex((ppmouseX+(ppmouseX-pppmouseX)*1/d0),(ppmouseY+(ppmouseY-pppmouseY)*1/d0), (ppmouseX + (ppmouseX-pppmouseX)*1/d1), (ppmouseY + (ppmouseY-pppmouseY)*1/d1),pmouseX, pmouseY);
   //bezierVertex(25, 125, 75, 140, 120, 120);
     endShape();
-    
   }
   pppmouseX = ppmouseX;
   pppmouseY = ppmouseY;
