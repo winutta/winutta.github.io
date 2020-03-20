02287239afa398d676b6953cba4f85980357f15c
winutta <willinutter@gmail.com>
2020-03-20 14:25:30 -0700

    Add files via upload
    shader background scene

---
+++ threeSketch.js
@@ -0,0 +1,68 @@
+//Takes care of necessities: Canvas, Renderer, Scene, Camera
+//Establishes the simplest renderering loop
+//Copy and add to this file
+
+function main(){
+	const canvas = document.getElementById("c");
+	const renderer = new THREE.WebGLRenderer({canvas,antialias:true});
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.setClearColor(0xE88D27,1);// a nice burnt orange color
+	const width = window.innerWidth;
+	const scene = new THREE.Scene();
+
+	const fov = 75;
+	const aspect =  window.innerWidth/window.innerHeight;
+
+	const near = 0.1;
+	const far = 2000;
+	const camera = new THREE.PerspectiveCamera(fov,aspect,near,far);
+	camera.position.z = 2;
+
+	var vFOV = camera.fov * Math.PI / 180;
+	var h = 2 * Math.tan( vFOV / 2 ) * camera.position.z;
+	var w = h * aspect;
+
+	const planeG = new THREE.PlaneGeometry(w,h,32);
+	const planeM = new THREE.ShaderMaterial({
+		uniforms: {
+			iTime: {type: 'f', value: 0.0},
+			res: {value: new THREE.Vector2(window.innerWidth,window.innerHeight)}	
+		},
+		vertexShader: document.getElementById("vertShader").textContent,
+		fragmentShader: document.getElementById("fragShader").textContent,
+	});
+
+	const plane = new THREE.Mesh(planeG,planeM);
+	scene.add(plane);
+
+	window.addEventListener( 'resize', onWindowResize, false );
+
+	function onWindowResize(){
+
+	planeM.uniforms.res.value = new THREE.Vector2(window.innerWidth,window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+	}
+
+
+
+
+
+	//Create objects/lights and add them to the scene here!
+
+	function render(time){
+		planeM.uniforms.iTime.value = time;
+		renderer.render(scene,camera);
+
+		requestAnimationFrame(render);
+	}
+
+	requestAnimationFrame(render);
+
+
+}
+
+main();
