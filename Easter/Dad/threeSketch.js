60223a89a7cd6448e068c4e21da493ecf223f56d
winutta <willinutter@gmail.com>
2020-04-11 15:55:21 -0700

    Update threeSketch.js

--- Easter/Dad/threeSketch.js
+++ Easter/Dad/threeSketch.js
@@ -153,7 +153,7 @@ function main(){
 	for(let i = 0; i<numPhotos;i){
 	
 		//var t0 = new THREE.TextureLoader().load( 'Lauren/'+i.toString()+'.' + photoTypes[i] ,function(){});
-		var t0 = new THREE.TextureLoader().load( 'dadFull/'+i.toString()+'.jpg' ,function(){});
+		var t0 = new THREE.TextureLoader().load( 'dadHalfR/'+i.toString()+'.jpg' ,function(){});
 
 		t0.minFilter = THREE.LinearFilter;
 		t0.magFilter = THREE.LinearFilter;
