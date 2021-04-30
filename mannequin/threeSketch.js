9eb6158c69ef8c866b76f81555384ceabb9b2c33
winutta <willinutter@gmail.com>
2021-04-30 15:41:43 -0700

    added mannequin

---
+++ mannequin/threeSketch.js
@@ -0,0 +1,631 @@
+
+
+var width = window.innerWidth, height = window.innerHeight;
+var points = [];
+
+
+function main(){
+	const canvas = document.getElementById("c");
+	const renderer = new THREE.WebGLRenderer({canvas,antialias:true, alpha: true});
+
+	var width = window.innerWidth, height = window.innerHeight;
+	renderer.setSize(window.innerWidth,window.innerHeight);
+	renderer.setClearColor(0xE88D27,0);// a nice burnt orange color
+	
+	const scene = new THREE.Scene();
+
+	const fov = 75;
+	const aspect = window.innerWidth/window.innerHeight;
+	const near = 0.01;
+	const far = 2000;
+
+	const camera = new THREE.PerspectiveCamera(fov,aspect,near,far);
+
+	camera.position.z = 2;
+
+	const controls = new THREE.OrbitControls( camera, renderer.domElement );
+	controls.update();
+
+
+	var vFOV = camera.fov * Math.PI / 180;
+
+	window.addEventListener( 'resize', onWindowResize, false );
+	function onWindowResize(){
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+	}
+	
+	// 1. Want to add Google Media Pipe API data and read it to the console, from video
+
+	const videoElement = document.getElementsByClassName('input-video')[0];
+
+	function onResults(results) {
+		points = results.poseLandmarks;
+	}
+
+	const pose = new Pose({locateFile: (file) => {
+	  return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
+	}});
+	pose.setOptions({
+	  upperBodyOnly: false,
+	  smoothLandmarks: true,
+	  minDetectionConfidence: 0.5,
+	  minTrackingConfidence: 0.5
+	});
+	pose.onResults(onResults);
+
+	const cameraGMP = new Camera(videoElement, {
+	  onFrame: async () => {
+	    await pose.send({image: videoElement});
+	  },
+	  width: width,
+	  height: height
+	});
+	cameraGMP.start();
+
+	// 2. Want to load the 3D Model and create sliders for the bones positions.
+
+	var sphereMaterial = new THREE.MeshBasicMaterial({color: "green", skinning: true});
+	var red = new THREE.MeshBasicMaterial({color: "red", skinning: true});
+	var manMat = new THREE.MeshBasicMaterial({color:"red", skinning: true});
+	// sphereMaterial.skinning = true;
+
+	var sphereMeshes = [];
+
+	for(var i=0; i< 33; i++){
+		var sphereGeometry = new THREE.SphereGeometry(0.01,32,32);
+		var sphere = new THREE.Mesh(sphereGeometry,sphereMaterial);
+		if (i == 14 ){
+			sphere = new THREE.Mesh(sphereGeometry,red);
+		}
+		sphere.position.set(i/10.,0,0);
+		sphereMeshes.push(sphere);
+		scene.add(sphere);
+	}
+
+	function makeSphere(){
+		var sphereGeometry = new THREE.SphereGeometry(0.01,32,32);
+		var sphere = new THREE.Mesh(sphereGeometry,sphereMaterial);
+		// sphere.position.set(i/10.,0,0);
+		// sphereMeshes.push(sphere);
+		scene.add(sphere);
+		return sphere;
+	}
+
+	const loader = new THREE.GLTFLoader();
+	var model;
+
+	function getAngleFromVectors(u,v){
+		var x_angle = Math.acos( math.dot( [u.y,u.z], [v.y,v.z] ) );
+		var y_angle = Math.acos( math.dot( [u.x,u.z], [v.x,v.z] ) );
+		var z_angle = Math.acos( math.dot( [u.x,u.y], [v.x,v.y] ) );
+
+		return new THREE.Vector3(x_angle,y_angle,z_angle);
+	}
+
+	var ik = new THREE.IK();
+	var ikChain = new THREE.IKChain();
+
+
+
+	loader.load(
+		// resource URL
+		"mann.glb",
+		// called when the resource is loaded
+		function ( gltf ) {
+			console.log("loading mann");
+			console.log(gltf.scene.children[2]);
+
+
+
+			model = gltf.scene.children[2];
+			// console.log(model.children[1].skeleton);
+			const helper = new THREE.SkeletonHelper(model);
+			// model.children[1].skeleton.bones[10].position.x = -10.1;
+			// model.children[1].material = manMat; //For some reason the mesh doesnt like sharing
+			scene.add(helper);
+			scene.add( model );
+			model.position.set(0,-1,0);
+			model.rotateX(Math.PI/20);
+			// model.scale.set(1,1,1);
+			// model.children[1].scale.set(0.1,0.1,0.1);
+
+			// console.log(model.children[1]);
+
+			// console.log(model.rotation);
+			// model.rotation.set(0,0,0);
+			model.updateMatrix();
+			model.updateMatrixWorld();
+
+
+
+			for(var i = 0; i < 63; i++){
+					var bone = model.children[1].skeleton.bones[i];
+					// console.log(bone);
+			}
+
+			// update bones not in a loop
+
+			var bone = model.children[1].skeleton.bones[7]; //55
+
+			var localPoint = new THREE.Vector3();
+
+			// localPoint.set(10,100,0);
+
+			var worldPoint = new THREE.Vector3().copy(localPoint);
+
+			bone.updateMatrixWorld();
+
+			var worldP2 = new THREE.Vector3();
+
+			bone.getWorldPosition(worldP2); // this one actually worked!
+
+			// bone.localToWorld(worldPoint); //local to world not consitent
+
+			var localP2 = new THREE.Vector3();
+			localP2.copy(worldP2);
+
+			bone.parent.worldToLocal(localP2);			
+
+			var worldP3 = new THREE.Vector3();
+			worldP3.copy(localP2);
+
+			bone.parent.localToWorld(worldP3); 
+
+			// bone.position.copy(localPoint);
+
+			// var s1 = makeSphere();
+
+			// s1.position.copy(worldPoint.multiplyScalar(1));
+			var s2 = makeSphere();
+
+			s2.position.copy(worldP2.multiplyScalar(1));
+
+
+			// console.log(localPoint,worldPoint, worldP2, bone.position, localP2, worldP3);
+
+
+
+
+
+			///////////////////////////////////
+
+			// Setup IK in render loop because points isn't available here //
+
+			// const constraints = [new THREE.IKBallConstraint(200)];
+
+			// var shoulder = model.children[1].skeleton.bones[32];
+			// var arm = model.children[1].skeleton.bones[33];
+			// var armPoint = sphereMeshes[14]; //Target
+			// var falseTarget = null;
+			// ikChain.add(new THREE.IKJoint(shoulder, { constraints }), { falseTarget });
+			// ikChain.add(new THREE.IKJoint(arm, { constraints }), { armPoint });
+
+			// ik.add(ikChain);
+
+			// scene.add(ik.getRootBone());
+
+
+		},
+		// called while loading is progressing
+		function ( xhr ) {
+
+			console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+		},
+		// called when loading has errors
+		function ( error ) {
+
+			console.log( 'An error happened' );
+
+		}
+	);
+
+	const color = 0xFFFFFF;
+	const intensity = 3;
+	const light = new THREE.AmbientLight(color, intensity);
+	scene.add(light);
+
+	var sphereFirst = true;
+
+	function updateSpheres(){
+
+		for(const [index,elem] of sphereMeshes.entries()){
+			var pVals = points[index];
+			var z = pVals.z +0.5;
+
+			var h =  2 * Math.tan( vFOV / 2 ) * (2 - ((1.-z) - 0.5));
+			var w = h * aspect;
+
+			if(sphereFirst){
+				sphereFirst = false;
+				console.log(w,h,"sphere");
+			}
+
+			elem.position.set((pVals.x-0.5)*w,h*((1.-pVals.y)-0.5),(1.-z)-0.5);
+		}
+
+	}
+
+
+
+	var bonesIndecies = [8,9,10,32,33,34,55,56,57,60,61,62,58,63];
+	var pointsIndecies = [11,13,15,12,14,16,23,25,27,24,26,28,31,32];
+
+	var first = true;
+
+	function getLocalPosition(bone,vec){
+		var localVec = vec.clone();
+		bone.parent.worldToLocal(localVec);
+		return localVec;
+	}
+
+	function getWorldPoint(point) {
+		var x = point.x;
+		var y = point.y;
+		var z = point.z+0.5;
+
+		var h =  2 * Math.tan( vFOV / 2 ) * (2 - ((1.-z) - 0.5));
+		var w = h * aspect;
+
+		var worldPoint = new THREE.Vector3();
+
+		var vals = {x: (x-0.5)*w, y: h*((1.-y)-0.5),z:((1.-z)-0.5)};
+		worldPoint.set(vals.x,vals.y,vals.z);
+
+		return worldPoint;
+	}
+
+	function updateSkinnedMesh(time){
+		if(model){
+
+
+			
+
+
+			
+			var lHip = getWorldPoint(points[23]);
+			var rHip = getWorldPoint(points[24]);
+
+			var lShoulder = getWorldPoint(points[11]);
+			var rShoulder = getWorldPoint(points[12]);
+
+			var avgShoulder = lShoulder.clone().add(rShoulder).multiplyScalar(0.5);
+
+			var avgHip = lHip.clone().add(rHip).multiplyScalar(0.5);
+
+			var diffAvgs = avgShoulder.clone().sub(avgHip).multiplyScalar(0.1);
+
+			var finalPosition = avgHip.clone().add(diffAvgs);
+
+			// console.log(avgHip);
+
+			var localAvgHip = getLocalPosition(model.children[1].skeleton.bones[0],finalPosition);
+
+			model.children[1].skeleton.bones[0].position.copy(localAvgHip);
+
+
+			// set rotations of arms and legs from triangles //
+
+			// SETTING UP for later lookAt //
+
+			var lElbow = getWorldPoint(points[13]);
+			var lWrist = getWorldPoint(points[15]);
+
+			var leftTopArm = lShoulder.clone().sub(lElbow);
+			var leftLowerArm = lWrist.clone().sub(lElbow);
+
+			var leftArmCross = leftTopArm.clone().cross(leftLowerArm);
+			leftArmCross.normalize();
+
+
+			var lShoulderBone = model.children[1].skeleton.bones[7];
+			var lElbowBone = model.children[1].skeleton.bones[8];
+			var lWristBone = model.children[1].skeleton.bones[9];
+
+			lShoulderBone.up = leftArmCross;
+			lElbowBone.up = leftArmCross;
+			lWristBone.up = leftArmCross;
+
+			//
+
+			var rElbow = getWorldPoint(points[14]);
+			var rWrist = getWorldPoint(points[16]);
+
+			var rightTopArm = rShoulder.clone().sub(rElbow);
+			var rightLowerArm = rWrist.clone().sub(rElbow);
+
+			var rightArmCross = rightTopArm.clone().cross(rightLowerArm);
+			rightArmCross.normalize().multiplyScalar(-1);
+
+			var rShoulderBone = model.children[1].skeleton.bones[31];
+			var rElbowBone = model.children[1].skeleton.bones[32];
+			var rWristBone = model.children[1].skeleton.bones[33];
+
+
+			rShoulderBone.up = rightArmCross;
+			rElbowBone.up = rightArmCross;
+			rWristBone.up = rightArmCross;
+
+			//
+
+			var rKnee = getWorldPoint(points[26]);
+			var rAnkle = getWorldPoint(points[28]);
+
+			var rightTopLeg = rHip.clone().sub(rKnee);
+			var rightLowerLeg = rAnkle.clone().sub(rKnee);
+
+			var rightLegCross = rightTopLeg.clone().cross(rightLowerLeg);
+			rightLegCross.normalize().multiplyScalar(1);
+
+			var rHipBone = model.children[1].skeleton.bones[60];
+			var rKneeBone = model.children[1].skeleton.bones[61];
+			var rAnkleBone = model.children[1].skeleton.bones[62];
+
+
+			rHipBone.up = rightLegCross;
+			rKneeBone.up = rightLegCross;
+			// rAnkleBone.up = rightLegCross;
+
+			//
+
+			var lKnee = getWorldPoint(points[25]);
+			var lAnkle = getWorldPoint(points[27]);
+
+			var leftTopLeg = lHip.clone().sub(lKnee);
+			var leftLowerLeg = lAnkle.clone().sub(lKnee);
+
+			var leftLegCross = leftTopLeg.clone().cross(leftLowerLeg);
+			leftLegCross.normalize().multiplyScalar(1);
+
+			var lHipBone = model.children[1].skeleton.bones[55];
+			var lKneeBone = model.children[1].skeleton.bones[56];
+			var lAnkleBone = model.children[1].skeleton.bones[57];
+
+
+			lHipBone.up = leftLegCross;
+			lKneeBone.up = leftLegCross;
+			// lAnkleBone.up = leftLegCross;
+
+
+
+
+
+		
+
+
+			for(var i = 0; i < 14; i++){
+				var boneInd = bonesIndecies[i];
+				var pointInd = pointsIndecies[i];
+
+				// var point = points[pointInd];
+				// var bone = model.children[1].skeleton.bones[boneInd];
+
+				var point = points[pointInd]; //23
+				var bone = model.children[1].skeleton.bones[boneInd]; //55
+
+				var x = point.x;
+				var y = point.y;
+				var z = point.z+0.5;
+
+				var h =  2 * Math.tan( vFOV / 2 ) * (2 - ((1.-z) - 0.5));
+				var w = h * aspect;
+
+
+
+
+				var worldPoint = new THREE.Vector3();
+
+				var vals = {x: (x-0.5)*w, y: h*((1.-y)-0.5),z:((1.-z)-0.5)};
+				worldPoint.set(vals.x,vals.y,vals.z);
+
+
+				// if(boneInd == 9 || boneInd == 10){
+					// bone.parent.lookAt(worldPoint);
+						
+				var boneList = [9,33,10,34,57,62,56,61];
+				boneList = [56,57,61,62, 9,10,33,34];
+				var leftArmBones = [7,8,9];
+
+
+				if(boneList.includes(boneInd)){
+					var rotatedBone = bone.parent;
+					rotatedBone.lookAt(worldPoint);
+					rotatedBone.rotateX(Math.PI/2);
+					// rotatedBone.rotateZ(Math.PI);
+
+
+					// worldPoint, bone.parent.getWorldPosition();
+					// if(worldPoint.z<bone.parent.getWorldPosition()){
+					// 	rotatedBone.rotateY(Math.PI);
+					// }
+
+					// if([9,10].includes(boneInd)) {
+					// 	// rotatedBone.rotateY(Math.PI/2);
+					// 	rotatedBone.rotateX(Math.PI/2);
+
+					// }
+
+					if([61,62,56,57].includes(boneInd)) {
+						rotatedBone.rotateY(Math.PI/2);
+						// rotatedBone.rotateX(Math.PI/2);
+
+					}
+
+					// if([34].includes(boneInd)) {
+					// 	// rotatedBone.rotateY(Math.PI/2);
+					// 	// rotatedBone.rotateX(Math.PI/2);
+
+					// }
+					// if(leftArmBones.includes(boneInd)){
+					// 	 //right side seems to need this., but left side doesnt
+					// } else {
+					// 	rotatedBone.rotateY(Math.PI/2);
+					// }
+					// rotatedBone.rotation.y = 0.;
+					// rotatedBone.rotation.y = Math.min(rotatedBone.rotation.y,Math.PI/2);
+
+
+
+					
+				}
+
+				// Left Arm triangle
+
+
+
+				// want to set root bone position.
+
+				
+
+				// if(model){
+				// 	var hipL = model.children[1].skeleton.bones[55];
+				// 	var pointHL = 
+				// 	var hipR = model.children[1].skeleton.bones[60];
+				// 	var shoulderL = model.children[1].skeleton.bones[7];
+				// 	var shoulderR = model.children[1].skeleton.bones[31];
+				// 	// hip.rotation.set(0,0,0);
+				// 	hip.lookAt(armPoint.position); //this would be the point for the child
+				// 	hip.rotateX(-Math.PI/2);
+				// 	hip.rotateZ(Math.PI);
+				// 	// hip.rotateY(Math.PI);
+				// }
+
+				// model.updateMatrixWorld();
+
+				var localPoint = new THREE.Vector3();
+
+				var localLength = bone.position.length();
+
+
+
+				//Want to save the length of the local vector
+				//Scale the resulting local vector to be unchanged;
+
+				localPoint.copy(worldPoint);
+
+				bone.parent.worldToLocal(localPoint);
+
+				// localPoint.setLength(localLength);
+
+				// Set Bone Position //
+				// if(boneInd != 33){
+					// bone.position.copy(localPoint);
+				// }
+
+				// }
+				// model.updateMatrixWorld();
+
+
+				////////////////////////////////
+
+				// Angle Setting Manually  //
+
+				// Need bone and bone parent world coords;
+				var boneWorld = new THREE.Vector3();
+				bone.getWorldPosition(boneWorld);
+				var parentWorld = new THREE.Vector3();
+				bone.parent.getWorldPosition(parentWorld);
+				var angle = getAngleFromVectors(boneWorld,parentWorld);
+
+				var eulerAngle = new THREE.Euler();
+				eulerAngle.setFromVector3(angle);
+				// This is the world angle though.. so probably want to transform it to local angle.
+				// Also angle needs to be an Euler object, not a Vector3 object.
+
+				// There are a lot of cases when you dont want to rotate towards the child object.
+
+				// bone.parent.lookAt(worldPoint);
+
+				// bone.parent.rotation.copy(eulerAngle);
+
+
+
+
+				
+
+				 //23
+				// var rightHip = model.children[1].skeleton.bones[60];
+				// var leftHip = model.children[1].skeleton.bones[55];
+				// var base= model.children[1].skeleton.bones[0];
+
+				// var rightPoint = points[24];
+				// var leftPoint = points[23];
+
+				// // var x = point.x;
+				// // var y = point.y;
+				// // var z = point.z+0.5;
+
+				// // var h =  2 * Math.tan( vFOV / 2 ) * (2 - ((1.-z) - 0.5));
+				// // var w = h * aspect;
+
+				// var valsRight = {x: (rightPoint.x-0.5)*w, y: h*((1.-rightPoint.y)-0.5),z:((1.-rightPoint.z)-0.5)};
+
+				// var rightWC = new THREE.Vector3(), leftWC = new THREE.Vector3();
+
+				// rightWC.copy(rightPoint);
+				// leftWC.copy(rightPoint);
+
+				// // need to apply the proper transofrm like above
+
+				// rightHip.parent.localToWorld(rightWC);
+				// leftHip.parent.localToWorld(leftWC);
+
+
+
+
+				// var mid = new THREE.Vector3().copy(rightWC);
+				// mid.add(leftWC).multiplyScalar(0.5);
+
+				// var baseLC = new THREE.Vector3().copy(mid);
+				// base.parent.worldToLocal(baseLC);
+
+				// // base.position.copy(baseLC);
+
+				// base.position.z = Math.sin(time)*4 - 105;
+
+				model.updateMatrixWorld();
+
+
+
+				if(first){
+					first = false;
+					// console.log(angle, baseLC, mid);
+					// console.log(prePoint, localPoint, midPoint, bone.position);
+
+				}
+				// var point = ;
+			}
+
+
+
+			// var bone = model.children[1].skeleton.bones[7];
+			// bone.position.x = Math.sin(time)*2;
+		}
+	}
+
+
+	function render(time){
+
+		if(points){
+			if(points[0]){
+				updateSpheres();
+				updateSkinnedMesh(time*0.01);
+			}
+		}
+
+		// ik.solve();
+
+		renderer.render(scene,camera);
+
+		requestAnimationFrame(render);
+	}
+
+	requestAnimationFrame(render);
+
+
+}
+
+main();
